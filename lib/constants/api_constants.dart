dd5a9a891ecc4931bd08d4d5dd56ad6355052349
karim sayed <76128546+karimsayed3@users.noreply.github.com>
2024-04-28 08:44:05 +0300

    last update

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+const String TMDB_API_KEY = "85ca61857083da71546e6ca143607b96";
+const String TMDB_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
